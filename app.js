e0bd1ab6ffb18fe3b1e1e94e7d5d22c175618b2d
Lukas Jakobsen <109243@RTS-365.dk>
2020-01-27 13:22:56 +0100

    router fix formidable

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require("express");
 var app = express();
 var router = require("./router");
 var { dbAuth } = require("./config/database");
+var formidable = require("./config/formidable");
 
 // config halløj
 dbAuth();
 app.use(router);
+router.use(formidable);
 
 module.exports = app;
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -9,8 +9,13 @@ function getSingleProduct(req, res, next) {
 	res.json()
 }
 
-function createProduct(req, res, next){
-	
+async function createProduct(req, res, next){
+	if(!req.fields){
+		res.status(400).end();
+		return;
+	}
+	let product = await Product.create(req.fields);
+	res.json(product);
 }
 
 module.exports = {
