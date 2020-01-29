79c6a9bf9bbcd9980396a83ae62c75dd5c7e1ac5
Lukas Jakobsen <109243@RTS-365.dk>
2020-01-29 11:25:16 +0100

    auto signout after 5 minutes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,12 +4,12 @@ var jwt = require("jsonwebtoken");
 var cache = require("memory-cache");
 var uuid = require("uuid/v1");
 
-async function getToken(req, res, next){
+async function getToken(req, res, next) {
     try {
         let token = await authenticateUser(req.fields.email, req.fields.password);
         let refreshToken = uuid();
-        cache.put(req.fields.email, refreshToken);
-        res.json({token, refreshToken});
+        cache.put(refreshToken, req.fields.email);
+        res.json({ token, refreshToken });
     } catch (error) {
         console.error(error)
         res.status(500).json({
@@ -18,21 +18,21 @@ async function getToken(req, res, next){
     }
 }
 
-async function authenticateUser(email, password){
+async function authenticateUser(email, password) {
     try {
         let user = await User.findOne({
             where: {
                 email: email
             }
         });
-        if(!user){
+        if (!user) {
             return Promise.reject("user not found")
         }
-        if (!bcrypt.compareSync(password, user.password)){
+        if (!bcrypt.compareSync(password, user.password)) {
             return Promise.reject("Email or password incorrect");
         }
         let userObject = { user: user.email }
-        let token = jwt.sign(userObject, process.env.JWT_SECRET);
+        let token = generateToken(userObject)
         return token;
     } catch (error) {
         console.error(error)
@@ -40,6 +40,44 @@ async function authenticateUser(email, password){
     }
 }
 
+async function refreshToken(req, res, next) {
+    if (!req.fields.refreshToken) {
+        res.status(400).end();
+        return;
+    }
+    let email = cache.get(req.fields.refreshToken)
+    cache.del(req.fields.refreshToken);
+    if (!email) {
+        res.status(404).end();
+        return;
+    }
+    let user = await User.findOne({
+        where: {
+            email
+        }
+    });
+    if (!user) {
+        res.status(404).end();
+        return;
+    }
+    let userObject = { user: user.email }
+    let refreshToken = uuid();
+    cache.put(refreshToken, user.email);
+    let token = generateToken(userObject)
+    res.json({
+        token,
+        refreshToken
+    });
+}
+
+function generateToken(userObject, exp = "300s"){
+    let token = jwt.sign({
+        data:userObject,
+         }, process.env.JWT_SECRET, { expiresIn: exp })
+         return token;
+}
+
 module.exports = {
-    getToken
+    getToken,
+    refreshToken
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,8 @@
-var { getToken } = require("../controllers/auth.controller")
+var { getToken, refreshToken } = require("../controllers/auth.controller")
 
 module.exports = function(router){
 
     router.post("/auth/get-token", getToken);
-
+    router.post("/auth/refresh-token", refreshToken);
 
 }; 
